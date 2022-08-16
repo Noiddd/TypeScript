2474dc1653d91ba285f846887d8e683b3113077c
Noiddd <dionang11@gmail.com>
2022-08-16 14:52:40 +0800

    1:07:00

--- typescript-course/src/App.tsx
+++ typescript-course/src/App.tsx
@@ -1,9 +1,32 @@
 import { Agent } from "https";
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-const App = () => {
-  return <div>Helllo World!</div>;
+import InputField from "./components/InputField";
+import { Todo } from "./model";
+import TodoList from "./components/TodoList";
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos((prevTodos) => {
+        return [...prevTodos, { id: Date.now(), todo, isDone: false }];
+      });
+      setTodo("");
+    }
+  };
+
+  return (
+    <div className="App">
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
+    </div>
+  );
 };
 
 export default App;
