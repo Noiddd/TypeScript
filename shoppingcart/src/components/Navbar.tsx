20ef7e0982bc85546b25d3bea04da619be218219
Noiddd <dionang11@gmail.com>
2022-08-18 20:15:08 +0800

    change

--- shoppingcart/src/components/Navbar.tsx
+++ shoppingcart/src/components/Navbar.tsx
@@ -1,5 +1,18 @@
+import { Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <NavbarBs className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav>
+          <Nav.link to="/" as={NavLink}>
+            Home
+          </Nav.link>
+        </Nav>
+      </Container>
+    </NavbarBs>
+  );
 };
 
 export default Navbar;
